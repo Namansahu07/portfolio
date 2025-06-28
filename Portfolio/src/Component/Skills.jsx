2b26622bc925128076f2df24512f77e83b321197
Naman <gfxquercus@gmail.com>
2025-06-29 02:08:37 +0530

    Improved UI and mobile responsiveness

--- Portfolio/src/Component/Skills.jsx
+++ Portfolio/src/Component/Skills.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+
+import './Skills.css';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div>
+      <h1>Skills</h1>
+    </div>
   )
-}
+};
 
-export default Skills
+export default Skills;
